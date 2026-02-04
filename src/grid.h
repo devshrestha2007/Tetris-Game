401102a1c64866f8d9084ed7019b3240bc2a6fed
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-04 23:18:27 +0545

    Grid-All
    Everything Related to Grid

--- src/grid.h
+++ src/grid.h
@@ -1,18 +1,23 @@
 #pragma once
-
+#include <vector>
+#include <raylib.h>
 class Grid
 {
     public:
     Grid();
    void Initialize();
    void Print();
+   void draw();
     int grid [30][40];
 
 
     private:
+   std:: vector<Color>GetCellColour();
     int numrow;
     int numcolumn;
     int cellsize;
+
+    std::vector <Color>colors;
     
 
 };
