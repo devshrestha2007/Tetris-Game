57b9f4fe792f0d654e3c5a0fb298930fb407fffc
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-04 23:19:28 +0545

    Size-color
    Size and Color of grid

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,12 +1,14 @@
 #include "grid.h"
 #include <iostream>
 using namespace std;
+#include  <raylib.h>>
 
 Grid::Grid(){
-numrow = 5;
-numcolumn = 5;
-cellsize =10;
+numrow = 10;
+numcolumn = 20;
+cellsize =30;
 Initialize();
+colors = GetCellColour();
 }
 
 void Grid::Initialize(){
@@ -28,3 +30,28 @@ void Grid::Initialize(){
     }
 
  }
+
+ vector<Color>Grid::GetCellColour(){
+const Color darkGrey = {26, 31, 40, 255};
+const Color green = {47, 230, 23, 255};
+const Color red = {232, 18, 18, 255};
+const Color orange = {226, 116, 17, 255};
+const Color yellow = {237, 234, 4, 255};
+const Color purple = {166, 0, 247, 255};
+const Color cyan = {21, 204, 209, 255};
+const Color blue = {13, 64, 216, 255};
+const Color lightBlue = {59, 85, 162, 255};
+const Color darkBlue = {44, 44, 127, 255};
+
+
+ return {darkGrey, green, red, orange, yellow, purple, cyan, blue};
+ }
+
+ void Grid::draw(){
+    for(int row = 0; row<numrow; row++ ){
+         for (int col =0 ; col<numcolumn; col++){
+           int cellvalue =  grid[row][col];
+            DrawRectangle(row*cellsize+1,col*cellsize+1,cellsize-1,cellsize-1,colors[cellvalue]);
+    }
+ }
+}
