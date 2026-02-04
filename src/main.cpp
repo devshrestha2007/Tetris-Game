e9bbdd380a97d0f8bd33a3d86e5fb92bfbd1435b
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-04 23:19:44 +0545

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,18 @@ int main()
     Grid grid = Grid();
  InitWindow(300,600,"Tertris");
  SetTargetFPS (60);
- Color lightgreen = {144, 238, 144};
+
+ Color darkblue = {44, 44, 127,255};
+ grid.grid[0][2] = 6;
  
  grid.Print();
+
  
  while (WindowShouldClose() == false){
+
     BeginDrawing();
-    ClearBackground (lightgreen);
+    ClearBackground (darkblue);
+     grid.draw();
     
     EndDrawing();
  }
