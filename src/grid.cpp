25217d2fb093d60ae87160653a12b2205b489a77
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-05 21:20:14 +0545

    Grid execution

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,7 +1,9 @@
 #include "grid.h"
 #include <iostream>
 using namespace std;
-#include  <raylib.h>>
+#include  <raylib.h>
+#include "Color.h"
+
 
 Grid::Grid(){
 numrow = 10;
@@ -31,21 +33,7 @@ void Grid::Initialize(){
 
  }
 
- vector<Color>Grid::GetCellColour(){
-const Color darkGrey = {26, 31, 40, 255};
-const Color green = {47, 230, 23, 255};
-const Color red = {232, 18, 18, 255};
-const Color orange = {226, 116, 17, 255};
-const Color yellow = {237, 234, 4, 255};
-const Color purple = {166, 0, 247, 255};
-const Color cyan = {21, 204, 209, 255};
-const Color blue = {13, 64, 216, 255};
-const Color lightBlue = {59, 85, 162, 255};
-const Color darkBlue = {44, 44, 127, 255};
 
-
- return {darkGrey, green, red, orange, yellow, purple, cyan, blue};
- }
 
  void Grid::draw(){
     for(int row = 0; row<numrow; row++ ){
