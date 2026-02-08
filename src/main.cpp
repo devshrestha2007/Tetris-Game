fbb5e7a9da137ea62aee366c7cee9b73a23c5d13
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-08 22:04:59 +0545

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,25 @@
+#pragma once
+#include <vector>
 #include <raylib.h>
-#include "grid.h"
 
-
-int main() 
+class Grid
 {
-    Grid grid = Grid();
- InitWindow(300,600,"Tertris");
- SetTargetFPS (60);
+public:
+    Grid();
+    void Initialize();
+    void Print();
+    void Draw();
+    bool IsCellOutside(int row, int column);
+    bool IsCellEmpty(int row, int column);
+    int ClearFullRows();
+    int grid[20][10];
 
- Color green = {144, 238, 144,255};
- grid.grid[0][2] = 6;
- 
- grid.Print();
-
- 
- while (WindowShouldClose() == false){
-
-    BeginDrawing();
-    ClearBackground (green);
-     grid.draw();
-    
-    EndDrawing();
- }
- CloseWindow();   
-}
+private:
+    bool IsRowFull(int row);
+    void ClearRow(int row);
+    void MoveRowDown(int row, int numRows);
+    int numRows;
+    int numCols;
+    int cellSize;
+    std::vector<Color> colors;
+};
