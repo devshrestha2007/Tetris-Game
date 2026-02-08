f185f2b06a2fbc437ca58a20f33986678ddef895
Dev Shrestha <devshrestha2007@gmail.com>
2026-02-08 22:11:47 +0545

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,58 @@
-#pragma once
-#include <vector>
 #include <raylib.h>
+#include "game.h"
+#include "colors.h"
+#include <iostream>
 
-class Grid
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
 {
-public:
-    Grid();
-    void Initialize();
-    void Print();
-    void Draw();
-    bool IsCellOutside(int row, int column);
-    bool IsCellEmpty(int row, int column);
-    int ClearFullRows();
-    int grid[20][10];
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
 
-private:
-    bool IsRowFull(int row);
-    void ClearRow(int row);
-    void MoveRowDown(int row, int numRows);
-    int numRows;
-    int numCols;
-    int cellSize;
-    std::vector<Color> colors;
-};
+int main()
+{
+    InitWindow(500, 620, "raylib Tetris");
+    SetTargetFPS(60);
+
+    Font font = LoadFontEx("Font/monogram.ttf", 64, 0, 0);
+
+    Game game = Game();
+
+    while (WindowShouldClose() == false)
+    {
+        UpdateMusicStream(game.music);
+        game.HandleInput();
+        if (EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
+
+        BeginDrawing();
+        ClearBackground(darkBlue);
+        DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
+        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
+        if (game.gameOver)
+        {
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        }
+        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
+
+        char scoreText[10];
+        sprintf(scoreText, "%d", game.score);
+        Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+
+        DrawTextEx(font, scoreText, {320 + (170 - textSize.x) / 2, 65}, 38, 2, WHITE);
+        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
+        game.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
